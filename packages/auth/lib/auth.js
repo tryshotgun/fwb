f6a384e5b66eeaa18e5cd98a4f71ddb95468f089
kevb10 <gasydev@gmail.com>
2021-03-13 16:42:16 -0500

    update response

--- packages/auth/lib/auth.js
+++ packages/auth/lib/auth.js
@@ -107,16 +107,14 @@ app.get('/callback', function (req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect('/#' +
-          querystring.stringify({
-            access_token: access_token,
-            refresh_token: refresh_token
-          }));
+        res.json({
+          access_token: access_token,
+          refresh_token: refresh_token
+        });
       } else {
-        res.redirect('/#' +
-          querystring.stringify({
-            error: 'invalid_token'
-          }));
+        res.json({
+          error: 'invalid_token'
+        });
       }
     });
   }
