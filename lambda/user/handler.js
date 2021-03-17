d3882d2fecf6af1e7bcc6e00d39c8e186ac85d49
kevb10 <gasydev@gmail.com>
2021-03-17 15:05:33 -0400

    update handler and add mock event

--- lambda/user/handler.js
+++ lambda/user/handler.js
@@ -1,31 +1,53 @@
-"use strict";
 const AWS = require('aws-sdk');
-const docClient = new AWS.DynamoDB.DocumentClient();;
-const tableName = "User";
+const axios = require('axios');
 
+const docClient = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' });
+
+const TABLE_NAME = 'User';
+const SPOTIFY_CURRENT_USER_INFO_URL = 'https://api.spotify.com/v1/me';
+
+/**
+ * Create a new user if user doesn't exist
+ * @throws ConditionalCheckFailedException if user exists
+*/
 module.exports.createNewUser = async (event) => {
   try {
-    const authToken = event.queryStringParameters.authorization_token;
+    let authToken;
+    if (event.queryStringParameters && event.queryStringParameters.authorization_token) {
+      authToken = event.queryStringParameters.authorization_token;
+    }
+
+    // Formulate header
+    const options = {
+      headers: { Authorization: `Bearer ${authToken}` },
+      json: true,
+    };
 
     // Create a query to Spotify API to grab basic info
-    const spotifyId;
-    const email;
-    const countryCode;
-    const profileImageURL;
+    const spotifyData = await axios.get(SPOTIFY_CURRENT_USER_INFO_URL, options)
+      .then((response) => response.data)
+      .catch((error) => error);
 
-    // Save to dynamoDB
+    const params = {
+      TableName: TABLE_NAME,
+      Item: {
+        spotify_id: +spotifyData.id,
+        country_code: spotifyData.country,
+        display_name: spotifyData.display_name,
+        email: spotifyData.email,
+        profile_image_url: spotifyData.images[0].url,
+        user_profile_url: spotifyData.external_urls.spotify,
+        created_dt_tm: String(Date.now()),
+      },
+      ConditionExpression: 'attribute_not_exists(spotify_id)',
+    };
 
+    // AWS dynamoDB operation
+    await docClient.put(params).promise();
 
     return {
       statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: 'Go Serverless v1.0! Your function executed successfully!',
-          input: event,
-        },
-        null,
-        2
-      ),
+      body: 'Success',
     };
   } catch (error) {
     return {
@@ -35,28 +57,50 @@ module.exports.createNewUser = async (event) => {
           message: error,
           input: event,
         },
-        null,
-        2
       ),
     };
   }
 };
 
+/** Get user information */
 module.exports.getUserInfo = async (event) => {
-  console.log(event);
+  try {
+    let authToken;
+    if (event.queryStringParameters && event.queryStringParameters.authorization_token) {
+      authToken = event.queryStringParameters.authorization_token;
+    }
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
+    const options = {
+      headers: { Authorization: `Bearer ${authToken}` },
+      json: true,
+    };
+
+    // use the access token to access the Spotify Web API
+    const spotifyData = await axios.get(SPOTIFY_CURRENT_USER_INFO_URL, options)
+      .then((response) => response.data)
+      .catch((error) => error);
+
+    // Save to dynamoDB
+    const result = JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: spotifyData,
         input: event,
       },
-      null,
-      2
-    ),
-  };
+    );
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    return {
+      statusCode: 200,
+      body: result,
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(
+        {
+          message: error,
+          input: event,
+        },
+      ),
+    };
+  }
 };
